c23bd7d0bf66ee45b5ba28819ebf0d35e106f4ac
Syed Zabiuddin <32957211+Zabi09@users.noreply.github.com>
2021-09-04 12:44:52 +0530

    settings page properly added

--- includes/admin/settings.php
+++ includes/admin/settings.php
@@ -1,178 +1,153 @@
-<h2>Core Web Vitals & PageSpeed Booster Settings</h2>
-
 <?php
-$active_tab = isset($_GET['tab']) ? $_GET['tab'] : "images";
 
-if (isset($_POST['submit'])) {
-        echo '<div class="notice notice-success is-dismissible"><p>Settings have been saved!</p></div>';
-    }
-?>
-<h2 class="nav-tab-wrapper">
-    <a href="?page=cwvpsb-images&tab=images" class="nav-tab <?php echo $active_tab == 'images' ? 'nav-tab-active' : ''; ?>">images</a>
-    <a href="?page=cwvpsb-images&tab=css" class="nav-tab <?php echo $active_tab == 'css' ? 'nav-tab-active' : ''; ?>">CSS</a>
-    <a href="?page=cwvpsb-images&tab=js" class="nav-tab <?php echo $active_tab == 'js' ? 'nav-tab-active' : ''; ?>">Javascript</a>
-    <a href="?page=cwvpsb-images&tab=cache" class="nav-tab <?php echo $active_tab == 'cache' ? 'nav-tab-active' : ''; ?>">Cache</a>
-</h2>
+add_action( 'admin_menu', 'cwvpsb_add_admin_menu' );
+add_action( 'admin_init', 'cwvpsb_settings_init' );
 
-<?php 
-    
-    switch ($active_tab) {
-        case 'images':
-            cwvpsb_image_optimization();
-            break;
-        case 'css':
-            cwvpsb_css_optimization();
-            break;
-        case 'js':
-            cwvpsb_js_optimization();
-            break;
-        case 'cache':
-            cwvpsb_cache_optimization();
-            break;       
-        default:
-            cwvpsb_image_optimization();
-    }
-    
-function cwvpsb_image_optimization() {
-    if (isset($_POST['submit'])) {
-        update_option('cwvpsb_check_webp', sanitize_text_field($_POST['check_webp']));
-        update_option('cwvpsb_check_lazyload', sanitize_text_field($_POST['check_ll']));
-    }
-    $check_webp = get_option('cwvpsb_check_webp');
-    $check_ll = get_option('cwvpsb_check_lazyload');
-    ?>
-    <form method="POST">
-        <?php wp_nonce_field('cwvpsb-nonce', 'cwvpsb-nonce-settings'); ?>
-        <table class="form-table" role="presentation">
-        <tbody>
-            <tr>
-                <th scope="row"><label>Webp images</label></th>
-                <td><label class="switch">
-                    <input name="check_webp" type="checkbox" value="1" <?php if ($check_webp) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Images are converted to WebP on the fly if the browser supports it. You don't have to do anything</p>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row"><label>Lazy Load</label></th>
-                <td><label class="switch">
-                    <input name="check_ll" type="checkbox" value="1" <?php if ($check_ll) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Lazy Load delays loading of images and iframes in long web pages. which are outside of viewport and will not be loaded before user scrolls to them</p>
-                </td>
-            </tr>
-        </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></br></br>
-            <input name="clear_site_cache_on_saved" type="submit" class="button-primary" value="Save Changes and Clear Site Cache" />
-        </p>
-    </form>
-<?php
-}
-function cwvpsb_css_optimization()
-{
-    if (isset($_POST['submit'])) {
-        update_option('cwvpsb_check_minification', sanitize_text_field($_POST['check_minify']));
-        update_option('cwvpsb_check_unused_css', sanitize_text_field($_POST['check_unused_css']));
-        update_option('cwvpsb_check_fonts', sanitize_text_field($_POST['cwvpsb_check_fonts']));
-    }
-    $check_minify = get_option('cwvpsb_check_minification'); 
-    $check_unused_css = get_option('cwvpsb_check_unused_css'); 
-    $check_fonts = get_option('cwvpsb_check_fonts'); 
-    ?>
-    <form method="POST">
-        <?php wp_nonce_field('cwvpsb-nonce', 'cwvpsb-nonce-settings'); ?>
-        <table class="form-table" role="presentation">
-        <tbody>
-            <tr>
-                <th scope="row"><label>Minification</label></th>
-                <td><label class="switch">
-                    <input name="check_minify" type="checkbox" value="1" <?php if ($check_minify) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">You will see the source of your HTML, CSS and JavaScript are now compressed and the size will be smaller which will be helpful to improve your page load speed.</p>
-                </td>
-            </tr>
-             <tr>
-                <th scope="row"><label>Remove Unused CSS</label></th>
-                <td><label class="switch">
-                    <input name="check_unused_css" type="checkbox" value="1" <?php if ($check_unused_css) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Makes your site even faster and lighter by automatically removing unused CSS from your website.</p>
-                </td>
-            </tr>
-             <tr>
-                <th scope="row"><label>Google Fonts Optimizations</label></th>
-                <td><label class="switch">
-                    <input name="cwvpsb_check_fonts" type="checkbox" value="1" <?php if ($check_fonts) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Locally hosting Google fonts for Pagespeed Insights or GT Metrix improvements.</p>
-                </td>
-            </tr>
-        </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></br></br>
-            <input name="clear_site_cache_on_saved" type="submit" class="button-primary" value="Save Changes and Clear Site Cache" />
-        </p>
-    </form>
-<?php
+function cwvpsb_add_admin_menu() { 
+    add_menu_page( 'Speed Booster', 'Speed Booster', 'manage_options', 'cwvpsb', 'cwvpsb_options_page' ,'dashicons-superhero');
 }
 
-function cwvpsb_js_optimization()
-{
-    if (isset($_POST['submit'])) {
-        update_option('cwvpsb_check_javascript_delay', sanitize_text_field($_POST['check_js']));
-    }
-    $check_js = get_option('cwvpsb_check_javascript_delay'); 
-    ?>
-    <form method="POST">
-        <?php wp_nonce_field('cwvpsb-nonce', 'cwvpsb-nonce-settings'); ?>
-        <table class="form-table" role="presentation">
-        <tbody>
-            <tr>
-                <th scope="row"><label>Delay JavaScript Execution</label></th>
-                <td><label class="switch">
-                    <input name="check_js" type="checkbox" value="1" <?php if ($check_js) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Delays the loading of JavaScript files until the user interacts like scroll, click etc, which improves performance</p>
-                </td>
-            </tr>
-        </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></br></br>
-            <input name="clear_site_cache_on_saved" type="submit" class="button-primary" value="Save Changes and Clear Site Cache" />
-        </p>
-    </form>
-<?php
+function cwvpsb_settings_init() { 
+
+    register_setting( 'plugin_page', 'cwvpsb_settings' );
+
+    add_settings_section(
+        'cwvpsb_plugin_page_section', 
+        __( '', 'cwvpsb' ), 
+        '', 
+        'plugin_page'
+    );
+
+    add_settings_field(
+        'cwvpsb_checkbox_webp', 
+        __( 'Webp images', 'cwvpsb' ), 
+        'cwvpsb_checkbox_webp_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_lazyload', 
+        __( 'Lazy Load', 'cwvpsb' ), 
+        'cwvpsb_checkbox_lazyload_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_minify', 
+        __( 'Minification', 'cwvpsb' ), 
+        'cwvpsb_checkbox_minify_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_unused_css', 
+        __( 'Remove Unused CSS', 'cwvpsb' ), 
+        'cwvpsb_checkbox_unused_css_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_fonts', 
+        __( 'Google Fonts Optimizations', 'cwvpsb' ), 
+        'cwvpsb_checkbox_fonts_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_delayjs', 
+        __( 'Delay JavaScript Execution', 'cwvpsb' ), 
+        'cwvpsb_checkbox_delayjs_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
+    add_settings_field(
+        'cwvpsb_checkbox_cache', 
+        __( 'Cache', 'cwvpsb' ), 
+        'cwvpsb_checkbox_cache_render', 
+        'plugin_page', 
+        'cwvpsb_plugin_page_section' 
+    );
 }
 
-function cwvpsb_cache_optimization()
-{
-    if (isset($_POST['submit'])) {
-        update_option('cwvpsb_check_cache', sanitize_text_field($_POST['check_cache']));
-    }
-    $check_cache = get_option('cwvpsb_check_cache'); 
+function cwvpsb_checkbox_webp_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
     ?>
-    <form method="POST">
-        <?php wp_nonce_field('cwvpsb-nonce', 'cwvpsb-nonce-settings'); ?>
-        <table class="form-table" role="presentation">
-        <tbody>
-            <tr>
-                <th scope="row"><label>Cache</label></th>
-                <td><label class="switch">
-                    <input name="check_cache" type="checkbox" value="1" <?php if ($check_cache) {echo "checked";} ?>>
-                    <span class="slider round"></span></label>
-                    <p class="description">Caching pages will reduce the response time of your site and your web pages load much faster, directly from cache.</p>
-                </td>
-            </tr>
-        </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></br></br>
-            <input name="clear_site_cache_on_saved" type="submit" class="button-primary" value="Save Changes and Clear Site Cache" />
-        </p>
-    </form>
-<?php
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_webp]' <?php checked( $options['cwvpsb_checkbox_webp'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Images are converted to WebP on the fly if the browser supports it. You don't have to do anything</p>
+    <?php
+}
+
+function cwvpsb_checkbox_lazyload_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_lazyload]' <?php checked( $options['cwvpsb_checkbox_lazyload'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Lazy Load delays loading of images and iframes in long web pages. which are outside of viewport and will not be loaded before user scrolls to them</p>
+    <?php
+}
+function cwvpsb_checkbox_minify_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_minify]' <?php checked( $options['cwvpsb_checkbox_minify'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">You will see the source of your HTML, CSS and JavaScript are now compressed and the size will be smaller which will be helpful to improve your page load speed</p>
+    <?php
+}
+function cwvpsb_checkbox_unused_css_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_unused_css]' <?php checked( $options['cwvpsb_checkbox_unused_css'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Makes your site even faster and lighter by automatically removing unused CSS from your website</p>
+    <?php
+}
+function cwvpsb_checkbox_fonts_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_fonts]' <?php checked( $options['cwvpsb_checkbox_fonts'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Locally hosting Google fonts for Pagespeed Insights or GT Metrix improvements</p>
+    <?php
+}
+function cwvpsb_checkbox_delayjs_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_delayjs]' <?php checked( $options['cwvpsb_checkbox_delayjs'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Delays the loading of JavaScript files until the user interacts like scroll, click etc, which improves performance</p>
+    <?php
+}
+function cwvpsb_checkbox_cache_render() {
+
+    $options = get_option( 'cwvpsb_settings' );
+    ?>
+    <label class="switch">
+    <input type='checkbox' name='cwvpsb_settings[cwvpsb_checkbox_cache]' <?php checked( $options['cwvpsb_checkbox_cache'], 1 ); ?> value='1'>
+    <span class="slider round"></span></label>
+    <p class="description">Caching pages will reduce the response time of your site and your web pages load much faster, directly from cache</p>
+    <?php
+}
+function cwvpsb_options_page() {?>
+        <form action='options.php' method='post'>
+            <h2>Core Web Vitals & PageSpeed Booster Settings</h2>
+            <?php
+            settings_fields( 'plugin_page' );
+            do_settings_sections( 'plugin_page' );
+            submit_button();
+            ?>
+        </form>
+        <?php
 }
--- includes/functions.php
+++ includes/functions.php
@@ -1,22 +1,7 @@
 <?php
 
-// Register settings menu
-function cwvpsb_register_menu() {
-    add_menu_page('Speed Booster', 'Speed Booster', 'manage_options', 'cwvpsb-images', 'cwvpsb_add_settings','dashicons-superhero');
-}
-add_action('admin_menu', 'cwvpsb_register_menu');
-
-// Settings page
-function cwvpsb_add_settings() {
-
-    // Validate nonce
-    if (isset($_POST['submit']) && !wp_verify_nonce($_POST['cwvpsb-nonce-settings'], 'cwvpsb-nonce')) {
-        echo '<div class="notice notice-error"><p>Nonce verification failed</p></div>';
-        exit;
-    }
-    // Add Settings Page
-    require_once CWVPSB_PLUGIN_DIR."includes/admin/settings.php";
-}
+// Add Settings Page
+require_once CWVPSB_PLUGIN_DIR."includes/admin/settings.php";
 
 add_filter('plugin_action_links_core-web-vitals-page-speed-booster/core-web-vitals-page-speed-booster.php', 'cwvpsb_add_settings_link');
 function cwvpsb_add_settings_link( $links ) {
@@ -25,32 +10,6 @@ function cwvpsb_add_settings_link( $links ) {
         '">' . 'Settings' . '</a>';
     return $links;
 }
-add_action('plugins_loaded', 'cwvpsb_set_default_settings');
-function cwvpsb_set_default_settings() {
-    if (get_option('cwvpsb_check_webp') === false){
-       update_option('cwvpsb_check_webp', 1);
-    }
-
-    if (get_option('cwvpsb_check_lazyload') === false){
-       update_option('cwvpsb_check_lazyload', 1);
-    }
-
-    if (get_option('cwvpsb_check_unused_css') === false){ 
-       update_option('cwvpsb_check_unused_css', 1);
-    }
-
-    if (get_option('cwvpsb_check_fonts') === false){ 
-       update_option('cwvpsb_check_fonts', 1);
-    }
- 
-    if (get_option('cwvpsb_check_minification') === false){
-       update_option('cwvpsb_check_minification', 1);
-    }
-   
-    if (get_option('cwvpsb_check_javascript_delay') === false){ 
-       update_option('cwvpsb_check_javascript_delay', 1);
-    }
-}
 
 function cwvpsb_complete_html_after_dom_loaded( $content ) {
     $content = apply_filters('cwvpsb_complete_html_after_dom_loaded', $content);
@@ -58,31 +17,26 @@ function cwvpsb_complete_html_after_dom_loaded( $content ) {
 }
 add_action('wp', function(){ ob_start('cwvpsb_complete_html_after_dom_loaded'); }, 999);
 
-add_action('plugins_loaded', 'cwvpsb_include_options_file' );
-function cwvpsb_include_options_file(){   
-    $check_webp = get_option('cwvpsb_check_webp');
-    if ($check_webp == true) {
+add_action('init', 'cwvpsb_include_options_file' );
+function cwvpsb_include_options_file(){ 
+    $options = get_option( 'cwvpsb_settings' );  
+    if ($options['cwvpsb_checkbox_webp'] == 1) {
        require_once CWVPSB_PLUGIN_DIR."includes/images/convert_webp.php";
     }
-    $check_minify = get_option('cwvpsb_check_minification');
-    if ($check_minify == true) {
+    if ($options['cwvpsb_checkbox_minify'] == 1) {
        require_once CWVPSB_PLUGIN_DIR."includes/css/minify.php";
     }
-    $check_js = get_option('cwvpsb_check_javascript_delay');
-    if ($check_js  == true) {
+    if ($options['cwvpsb_checkbox_delayjs'] == 1) {
        require_once CWVPSB_PLUGIN_DIR."includes/javascript/delay_js.php";
     }
-    $check_ll = get_option('cwvpsb_check_lazyload');
-    if ($check_ll  == true) {
+    if ($options['cwvpsb_checkbox_lazyload'] == 1) {
        require_once CWVPSB_PLUGIN_DIR."includes/images/lazy-loading.php";
     }
-    $check_unused_css = get_option('cwvpsb_check_unused_css');
-    if ($check_unused_css == true) {
-    require_once CWVPSB_PLUGIN_DIR."includes/css/unused_css.php";
+    if ($options['cwvpsb_checkbox_unused_css'] == 1) {
+        require_once CWVPSB_PLUGIN_DIR."includes/css/unused_css.php";
     }
-    $check_fonts = get_option('cwvpsb_check_fonts');
-    if ($check_fonts == true) {
-    require_once CWVPSB_PLUGIN_DIR."includes/css/google-fonts.php";
+    if ($options['cwvpsb_checkbox_fonts'] == 1) {
+        require_once CWVPSB_PLUGIN_DIR."includes/css/google-fonts.php";
     }
 }
 
