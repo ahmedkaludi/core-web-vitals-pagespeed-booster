64ac3e9e5fe298d06a266317974bcd6283822fa4
Ajeet kumar <ajeet.gupta51@gmail.com>
2022-03-30 07:01:14 +0530

    Added css lazyload

--- includes/css/critical_css.php
+++ includes/css/critical_css.php
@@ -6,13 +6,21 @@ class criticalCss{
 	}
 
 	public function init(){
+		if ( function_exists('is_checkout') && is_checkout() ) {
+        	return $html;
+	    }
+	    if ( function_exists('elementor_load_plugin_textdomain') && \Elementor\Plugin::$instance->preview->is_preview_mode() ) {
+	    	return;
+		}
+		add_action('wp', array($this, 'delay_css_loadings'));
+		add_action('wp_footer', array($this,'cwvpsb_delay_js_load'), PHP_INT_MAX);
+
 		add_action('wp_head', array($this, 'print_style_cc'));
 		if(!is_admin()){
 			add_action( 'wp_enqueue_scripts', array($this, 'scripts_styles') );
-			
 		}
 		add_action("wp_ajax_cc_call", array($this, 'grab_cc_css'));
-			add_action("wp_ajax_nopriv_cc_call", array($this, 'grab_cc_css'));
+		add_action("wp_ajax_nopriv_cc_call", array($this, 'grab_cc_css'));
 	}
 
 	function scripts_styles(){
@@ -85,5 +93,117 @@ class criticalCss{
 		}
 	}
 	
+	public function delay_css_loadings(){
+		add_filter('cwvpsb_complete_html_after_dom_loaded', array($this, 'cwvpsb_delay_css_html'), 2,1);
+	}
+
+	public function cwvpsb_delay_css_html($html){
+		$html_no_comments = preg_replace('/<!--(.*)-->/Uis', '', $html);
+		preg_match_all('/<link\s?([^>]+)?>/is', $html_no_comments, $matches);
+
+		if(!isset($matches[0])) {
+			return $html;
+		}
+		
+		foreach($matches[0] as $i => $tag) {
+			$atts_array = !empty($matches[1][$i]) ? cwvpsb_get_atts_array($matches[1][$i]) : array();
+			if(isset($atts_array['rel']) && stripos($atts_array['rel'], 'stylesheet') === false) {
+				continue;
+			}
+			$delay_flag = false;
+			$excluded_scripts = array(
+				'cwvpsb-delayed-styles',
+			);
+
+			if(!empty($excluded_scripts)) {
+				foreach($excluded_scripts as $excluded_script) {
+					if(strpos($tag, $excluded_script) !== false) {
+						continue 2;
+					}
+				}
+			}
+
+			$delay_flag = true;
+			if(!empty($atts_array['rel'])) {
+				$atts_array['data-cwvpsb-rel'] = $atts_array['rel'];
+			}
+
+			$atts_array['rel'] = 'cwvpsbdelayedstyle';
+			$atts_array['defer'] = 'defer';
+		
+			if($delay_flag) {
+				//$html = str_replace($tag, '<noscript id="cwvpsbdelayedstyle">'.$tag.'</noscript>', $html);
+				 $delayed_atts_string = cwvpsb_get_atts_string($atts_array);
+		        $delayed_tag = sprintf('<link %1$s', $delayed_atts_string) . (!empty($matches[3][$i]) ? $matches[3][$i] : '') .'/>';
+				$html = str_replace($tag, $delayed_tag, $html); 
+				continue;
+			}
+		}
+		return $html;
+	}
+
+	function cwvpsb_get_atts_string($atts_array) {
+
+		if(!empty($atts_array)) {
+			$assigned_atts_array = array_map(
+			function($name, $value) {
+				if($value === '') {
+					return $name;
+				}
+				return sprintf('%s="%s"', $name, esc_attr($value));
+			},
+				array_keys($atts_array),
+				$atts_array
+			);
+			$atts_string = implode(' ', $assigned_atts_array);
+			return $atts_string;
+		}
+		return false;
+	}
+
+	function cwvpsb_get_atts_array($atts_string) {
+
+		if(!empty($atts_string)) {
+			$atts_array = array_map(
+				function(array $attribute) {
+					return $attribute['value'];
+				},
+				wp_kses_hair($atts_string, wp_allowed_protocols())
+			);
+			return $atts_array;
+		}
+		return false;
+	}
+
+
+	function cwvpsb_delay_js_load() {
+  	echo '<script type="text/javascript" id="cwvpsb-delayed-styles">
+	(function() {
+	cwvpsbUserInteractionsAll = ["keydown", "mousemove", "wheel", "touchmove", "touchstart", "touchend", "touchcancel", "touchforcechange"]
+	cwvpsbUserInteractionsAll.forEach(function(e) {
+			window.removeEventListener(e, cwvpsbTriggerDOMListener, {
+            passive: !0
+        })
+	}), "loading" === document.readyState ? document.addEventListener("DOMContentLoaded", ctl) : ctl()
+	function ctl(){
+		var cssEle = document.querySelectorAll("link[rel=cwvpsbdelayedstyle]");
+		console.log(cssEle.length);
+		for(var i=0; i <= cssEle.length;i++){
+			if(cssEle[i]){
+				var cssMain = document.createElement("link");
+				cssMain.href = cssEle[i].href;
+				cssMain.rel = "stylesheet";
+				cssMain.type = "text/css";
+				document.getElementsByTagName("head")[0].appendChild(cssMain);
+			}
+		}
+	}
+
+	 
+
+})();
+</script>';
+}
+
 }
 $cwvpbCriticalCss = new criticalCss();
