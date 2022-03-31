26f42a00c141a5d7cf6b535c8d850561cd53b6a2
Ajeet kumar <ajeet.gupta51@gmail.com>
2022-03-31 14:02:48 +0530

    #42 CSS minify should not work on feed
    #42 CSS minify should not work on feed

--- includes/css/minify.php
+++ includes/css/minify.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 add_filter('cwvpsb_complete_html_after_dom_loaded','cwvpsb_minify_html');
 function cwvpsb_minify_html($input){
- 
+    if(function_exists('is_feed')&& is_feed()){return $input;}
     if(trim($input) === "") return $input;
     // Remove extra white-space(s) between HTML attribute(s)
     $input = preg_replace_callback('#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s', function($matches) {
