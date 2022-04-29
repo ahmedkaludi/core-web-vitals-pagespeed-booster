9f95dae2944bef87a21b38fbd2922cf221c4b87a
Ajeet kumar <ajeet.gupta51@gmail.com>
2022-04-29 12:25:03 +0530

    Proper minify codes
    Proper minify codes

--- includes/css/minify.php
+++ includes/css/minify.php
@@ -10,10 +10,12 @@ function cwvpsb_minify_html($input){
     }
     if(function_exists('is_feed')&& is_feed()){return $input;}
     if(trim($input) === "") return $input;
+    $input = cwvpsb_minify_html_output($input);
+    return $input;
     // Remove extra white-space(s) between HTML attribute(s)
     $input = preg_replace_callback('#<([^\/\s<>!]+)(?:\s+([^<>]*?)\s*|\s*)(\/?)>#s', function($matches) {
         return '<' . $matches[1] . preg_replace('#([^\s=]+)(\=([\'"]?)(.*?)\3)?(\s+|$)#s', ' $1$2', $matches[2]) . $matches[3] . '>';
-    }, str_replace(array("\r", "\n"), array("", ""), $input));//str_replace(array("\r", "\n"), array("", ""), $input)
+    }, str_replace(array("\r"), array(""), $input));//str_replace(array("\r", "\n"), array("", ""), $input)
     
     return preg_replace(
         array(
@@ -30,7 +32,8 @@ function cwvpsb_minify_html($input){
             '#(&nbsp;)&nbsp;(?![<\s])#', // clean up ...
             '#(?<=\>)(&nbsp;)(?=\<)#', // --ibid
             // Remove HTML comment(s) except IE comment(s)
-            '#\s*<!--(?!\[if\s).*?-->\s*|(?<!\>)\n+(?=\<[^!])#s'
+            '#\s*<!--(?!\[if\s).*?-->\s*|(?<!\>)\n+(?=\<[^!])#s',
+            '#^([\t| ]*)\/\/.*\n#m'
         ),
         array(
             '<$1$2</$1>',
@@ -42,7 +45,72 @@ function cwvpsb_minify_html($input){
             '<$1$2',
             '$1 ',
             '$1',
+            "",
             ""
         ),
     $input);
+}
+
+
+function cwvpsb_minify_html_output($buffer) {
+    if ( substr( ltrim( $buffer ), 0, 5) == '<?xml' )
+        return ( $buffer );
+    $minify_javascript = 'no';;
+    $minify_html_comments = 'yes';
+    $minify_html_utf8 = 'yes';
+    if ( $minify_html_utf8 == 'yes' && mb_detect_encoding($buffer, 'UTF-8', true) )
+        $mod = '/u';
+    else
+        $mod = '/s';
+    $buffer = str_replace(array (chr(13) . chr(10), chr(9)), array (chr(10), ''), $buffer);
+    $buffer = str_ireplace(array ('<script', '/script>', '<pre', '/pre>', '<textarea', '/textarea>', '<style', '/style>'), array ('M1N1FY-ST4RT<script', '/script>M1N1FY-3ND', 'M1N1FY-ST4RT<pre', '/pre>M1N1FY-3ND', 'M1N1FY-ST4RT<textarea', '/textarea>M1N1FY-3ND', 'M1N1FY-ST4RT<style', '/style>M1N1FY-3ND'), $buffer);
+    $split = explode('M1N1FY-3ND', $buffer);
+    $buffer = ''; 
+    for ($i=0; $i<count($split); $i++) {
+        $ii = strpos($split[$i], 'M1N1FY-ST4RT');
+        if ($ii !== false) {
+            $process = substr($split[$i], 0, $ii);
+            $asis = substr($split[$i], $ii + 12);
+            if (substr($asis, 0, 7) == '<script') {
+                $split2 = explode(chr(10), $asis);
+                $asis = '';
+                for ($iii = 0; $iii < count($split2); $iii ++) {
+                    if ($split2[$iii])
+                        $asis .= trim($split2[$iii]) . chr(10);
+                    if ( $minify_javascript != 'no' )
+                        if (strpos($split2[$iii], '//') !== false && substr(trim($split2[$iii]), -1) == ';' )
+                            $asis .= chr(10);
+                }
+                if ($asis)
+                    $asis = substr($asis, 0, -1);
+                if ( $minify_html_comments != 'no' )
+                    $asis = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $asis);
+                if ( $minify_javascript != 'no' )
+                    $asis = str_replace(array (';' . chr(10), '>' . chr(10), '{' . chr(10), '}' . chr(10), ',' . chr(10)), array(';', '>', '{', '}', ','), $asis);
+            } else if (substr($asis, 0, 6) == '<style') {
+                $asis = preg_replace(array ('/\>[^\S ]+' . $mod, '/[^\S ]+\<' . $mod, '/(\s)+' . $mod), array('>', '<', '\\1'), $asis);
+                if ( $minify_html_comments != 'no' )
+                    $asis = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $asis);
+                $asis = str_replace(array (chr(10), ' {', '{ ', ' }', '} ', '( ', ' )', ' :', ': ', ' ;', '; ', ' ,', ', ', ';}'), array('', '{', '{', '}', '}', '(', ')', ':', ':', ';', ';', ',', ',', '}'), $asis);
+            }
+        } else {
+            $process = $split[$i];
+            $asis = '';
+        }
+        $process = preg_replace(array ('/\>[^\S ]+' . $mod, '/[^\S ]+\<' . $mod, '/(\s)+' . $mod), array('>', '<', '\\1'), $process);
+        if ( $minify_html_comments != 'no' )
+            $process = preg_replace('/<!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->' . $mod, '', $process);
+        $buffer .= $process.$asis;
+    }
+    $buffer = str_replace(array (chr(10) . '<script', chr(10) . '<style', '*/' . chr(10), 'M1N1FY-ST4RT'), array('<script', '<style', '*/', ''), $buffer);
+    $minify_html_xhtml = 'yes';
+    $minify_html_relative = 'yes';
+    $minify_html_scheme = 'yes';
+    if ( $minify_html_xhtml == 'yes' && strtolower( substr( ltrim( $buffer ), 0, 15 ) ) == '<!doctype html>' )
+        $buffer = str_replace( ' />', '>', $buffer );
+    if ( $minify_html_relative == 'yes' )
+        $buffer = str_replace( array ( 'https://' . $_SERVER['HTTP_HOST'] . '/', 'http://' . $_SERVER['HTTP_HOST'] . '/', '//' . $_SERVER['HTTP_HOST'] . '/' ), array( '/', '/', '/' ), $buffer );
+    if ( $minify_html_scheme == 'yes' )
+        $buffer = str_replace( array( 'http://', 'https://' ), '//', $buffer );
+    return ($buffer);
 }
