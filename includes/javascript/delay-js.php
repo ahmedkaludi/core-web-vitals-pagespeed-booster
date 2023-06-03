31a83aaa4f3ec34c4b76e65ebe4eda0d6b3a2955
Akshay Wali <akshaywali@live.in>
2023-06-03 15:56:41 +0530

    Update delay-js.php

--- includes/javascript/delay-js.php
+++ includes/javascript/delay-js.php
@@ -632,7 +632,7 @@ function cwvpsb_delay_js_load() {
                 let gres = uag.match(gpat);
                 let cpat = /Chrome-Lighthouse/gm;
                 let cres = uag.match(cpat);
-                let wait_till=1000;
+                let wait_till=300;
 				let new_ua = "Mozilla/5.0 (Linux; Android 11; moto g power (2022)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36";
                 if(gres || cres || uag==new_ua){
                     wait_till = 3000;
@@ -646,10 +646,9 @@ function cwvpsb_delay_js_load() {
 			
 			window.addEventListener("load", function(e) {
 				console.log("load complete");
-				ccfw_loaded=true;
 				 setTimeout(function(){
 					calculate_load_times();
-				 },1000);
+				 },800);
 		 });
 
 			async function cwvpsbTriggerDelayedScripts() {
