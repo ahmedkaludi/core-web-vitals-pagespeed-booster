44c870310c43ce73ee983cd444944cd1f1800751
Mustak M <developer.mustak@gmail.com>
2023-10-19 16:40:05 +0530

    #111
    Fixed the issue to remove url table for multisite.

--- uninstall.php
+++ uninstall.php
@@ -11,5 +11,18 @@ if ( ! defined( 'CWVPSB_IMAGE_DIR' ) ) {
 }
 
 global $wpdb, $table_prefix;
-$cached_table = $table_prefix . 'cwvpb_critical_urls';
-$wpdb->query("DROP TABLE `$cached_table`");
+if (function_exists('is_multisite') && is_multisite()) {	
+	$original_blog_id = get_current_blog_id();
+	$blog_ids = $wpdb->get_col("SELECT blog_id FROM {$wpdb->blogs}");
+
+	foreach ($blog_ids as $blog_id) {
+	    switch_to_blog($blog_id);
+	    $cached_table = $table_prefix . 'cwvpb_critical_urls';
+	    $wpdb->query("DROP TABLE `$cached_table`");
+	}
+
+	switch_to_blog($original_blog_id);
+} else {
+	$cached_table = $table_prefix . 'cwvpb_critical_urls';
+	$wpdb->query("DROP TABLE $cached_table");
+}
