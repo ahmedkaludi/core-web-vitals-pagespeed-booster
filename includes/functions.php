f07a1bd0bf537fdb047b9685d924d1323f7a068b
Akshay Wali <akshaywali@live.in>
2023-10-26 15:02:20 +0530

    Robots.txt error appears when you we enable our CWV plugin. #114

--- includes/functions.php
+++ includes/functions.php
@@ -19,7 +19,9 @@ function cwvpsb_add_settings_link( $links ) {
 }
 
 function cwvpsb_complete_html_after_dom_loaded( $content ) {
-     if(function_exists('is_feed')&& is_feed()){return $content;}
+    global $wp;
+    if(strpos($wp->request,'robots.txt')!==false){return $content;}
+    if(function_exists('is_feed')&& is_feed()){return $content;}
     $content = apply_filters('cwvpsb_complete_html_after_dom_loaded', $content);
     return $content;
 }
